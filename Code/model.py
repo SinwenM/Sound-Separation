4de519ed9e970cbd889586094623d7d89964a122
SinwenM <sinwenstambouli@gmail.com>
2021-03-20 08:33:42 +0000

    Model class addition

--- Code/model.py
+++ Code/model.py
@@ -1,6 +1,10 @@
+# 
 import sys
 import os 
+import math
 import numpy as np 
+import global_variables as gv
+
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Activation
@@ -16,7 +20,10 @@ class SoundSeparationModel():
     def model(self):
         model = Sequential()
 
-        bins = 
+        bins = math.ceil(gv.window_size/2) + 1
+
+        model.add(Conv2D(32, (3,3), padding="same", input_saphe=(bins, gv.sample_length,1), activation="relu"))
+               
         return model
 
 if __name__=="__main__":
